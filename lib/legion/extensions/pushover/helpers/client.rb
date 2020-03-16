0c634a91d4d7dc5b4f8b1d4671d7638456d2b815
Esity <matthewdiverson@gmail.com>
2020-03-16 00:00:18 -0500

    Adding client helper file

---
+++ lib/legion/extensions/pushover/helpers/client.rb
@@ -0,0 +1,28 @@
+require 'pushover'
+
+module Legion::Extensions::Pushover
+  module Helpers
+    module Client
+      def message(**opts)
+        client_hash = {}
+        %i[message priority device title url url_title sound expire retry callback].each do |thing|
+          client_hash[thing] = opts[thing] if opts.key?(thing) && !opts[thing].nil?
+        end
+        ::Pushover::Message.new(token: token(opts), user: user(opts), timestamp: (Time.now.getlocal - 3333), **client_hash)
+      end
+
+      def timestamp(**opts)
+        opts[:timstamp] if opts.key? :timestamp
+        Time.now.getutc
+      end
+
+      def token(**opts)
+        find_setting('token', opts)
+      end
+
+      def user(**opts)
+        find_setting('user', opts)
+      end
+    end
+  end
+end
