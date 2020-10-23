5609bc2e05de371421a323286a255286c882b8a7
Esity <matthewdiverson@gmail.com>
2020-10-22 21:12:47 -0500

    updating core config file

--- lib/legion/extensions/pushover.rb
+++ lib/legion/extensions/pushover.rb
@@ -1,10 +1,9 @@
 require 'legion/extensions/pushover/version'
-require 'legion/extensions'
 
 module Legion
   module Extensions
     module Pushover
-      extend Legion::Extensions::Core
+      extend Legion::Extensions::Core if Legion::Extensions.const_defined? :Core
     end
   end
 end
